ed9c01b42af4d64803f2885889c41bd91f324aff
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-02-26 13:09:54 +0100

    Created the class CarCategory. Created CRUD for CarCategory.

--- FribergCarRentalsBravo.Data/DatabaseContexts/ApplicationDbContext.cs
+++ FribergCarRentalsBravo.Data/DatabaseContexts/ApplicationDbContext.cs
@@ -22,38 +22,13 @@ namespace FribergCarRentalsBravo.DataAccess.DatabaseContexts
         }
 
         #endregion
-
+        
         #region DBSets
 
-        public DbSet<CarEntity> Cars { get; set; }
+        public DbSet<CarCategory> CarCategories { get; set; }
 
         #endregion
 
-        #region Methods
 
-        /// <summary>
-        /// Configures the options.
-        /// </summary>
-        /// <param name="optionsBuilder"></param>
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
-            {
-                optionsBuilder.EnableSensitiveDataLogging(sensitiveDataLoggingEnabled: true);
-            }
-        }
-
-        /// <summary>
-        /// Configures the database model.
-        /// </summary>
-        /// <param name="modelBuilder">The model builder.</param>
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
-
-        #endregion
     }
 }
