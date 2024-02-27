0bd8a3991c65661251e146ed22786c1c7bad44d5
MarcusMellberg <147613853+MarcusMellberg@users.noreply.github.com>
2024-02-27 14:21:50 +0100

    Marcus (#5)
    * Created the class CarCategory.
    Created CRUD for CarCategory.
    
    * Update ApplicationDbContext.cs
    
    Added deleted methods.
    
    * Naming convention structure
    
    * Build a CustomerController to handle Customer functions for customers.
    
    ---------
    
    Co-authored-by: kras12 <runehero@hotmail.com>

---
+++ FribergCarRentalsBravo/Controllers/Customers/CustomerController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FribergCarRentalsBravo.DataAccess.DatabaseContexts;
+using FribergCarRentalsBravo.DataAccess.Entities;
+using FribergCarRentalsBravo.DataAccess.Repositories;
+
+namespace FribergCarRentalsBravo.Controllers.Customers
+{
+    public class CustomerController : Controller
+    {
+        public ICustomerRepository customerRep { get; }
+
+        public CustomerController(ICustomerRepository customerRep)
+        {
+            this.customerRep = customerRep;
+        }
+
+        // GET: CustomerController
+        public async Task<ActionResult> Index()
+        {
+            var customers = await customerRep.GetAllCustomers();
+            return View(customers);
+        }
+
+        // GET: CustomerController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await customerRep.GetCustomerById(id));
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await customerRep.CreateCustomer(customer);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == null || customerRep.GetAllCustomers == null)
+            {
+                return NotFound();
+            }
+
+            Customer customer = await customerRep.GetCustomerById(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await customerRep.EditCustomer(customer);
+                }
+                catch (Exception)
+                {
+                    return View();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        // GET: CustomerController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Customer customer = await customerRep.GetCustomerById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: CustomerController/Delete/5             
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await customerRep.GetCustomerById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await customerRep.DeleteCustomer(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Det gick inte att ta bort kunden. Försök igen senare.");
+                return View(customer);
+            }
+        }
+    }
+}
